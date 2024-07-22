e37498dfdf9a890feabc1b5d00253a1486bc768a
Code-Web-Dav <720821104091@hit.edu.in>
2024-07-22 16:32:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const a = document.getElementById("A")
+const b = document.getElementById("B")
+const c = document.getElementById("C")
+const d = document.getElementById("D")
+function D(){
+    d.style.backgroundColor = "rgb(30, 221, 30)"
+    d.style.color = "whitesmoke"
+}
+function A(){
+    a.style.backgroundColor = "red"
+    a.style.color = "whitesmoke"
+}
+function C(){
+    c.style.backgroundColor = "red"
+    c.style.color = "whitesmoke"
+}
+function B(){
+    b.style.backgroundColor = "red"
+    b.style.color = "whitesmoke"
+}
